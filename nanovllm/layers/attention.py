3efa43f4cc1e50cd657c7e79e11c5e66d8f6b6e1
hanzhi.421 <hanzhi.421@bytedance.com>
2025-08-12 20:44:20 +0800

    chore: add docs about sample

--- nanovllm/layers/attention.py
+++ nanovllm/layers/attention.py
@@ -72,22 +72,24 @@ class Attention(nn.Module):
 
     def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
         o: torch.Tensor
-        q = q.view(-1, self.num_heads, self.head_dim)
+        q = q.view(-1, self.num_heads, self.head_dim)       # tensor reshaping
         k = k.view(-1, self.num_kv_heads, self.head_dim)
         v = v.view(-1, self.num_kv_heads, self.head_dim)
         context = get_context()
         k_cache, v_cache = self.k_cache, self.v_cache
-        if k_cache.numel() and v_cache.numel():
+        if k_cache.numel() and v_cache.numel():     # 非空则存储
             store_kvcache(k, v, k_cache, v_cache, context.slot_mapping)
+        # 
         if context.is_prefill:
             if context.block_tables is not None:    # prefix cache
+                # 这里表示的是第K轮对话的prefill阶段（k>1)，也就是虽然他是prefill，但并不是第一轮对话那种啥也没有的情况。
                 k, v = k_cache, v_cache
             o = flash_attn_varlen_func(q, k, v,
                                        max_seqlen_q=context.max_seqlen_q, cu_seqlens_q=context.cu_seqlens_q,
                                        max_seqlen_k=context.max_seqlen_k, cu_seqlens_k=context.cu_seqlens_k,
                                        softmax_scale=self.scale, causal=True, block_table=context.block_tables)
         else:    # decode
-            o = flash_attn_with_kvcache(q.unsqueeze(1), k_cache, v_cache,
+            o = flash_attn_with_kvcache(q.unsqueeze(1), k_cache, v_cache,       # 这里就不需要再计算k，v了，而是用cache代替
                                         cache_seqlens=context.context_lens, block_table=context.block_tables, 
                                         softmax_scale=self.scale, causal=True)
         o = o.view(-1, self.num_heads * self.head_dim)
--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -8,11 +8,26 @@ class Sampler(nn.Module):
         super().__init__()
 
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
+        # 采样
         logits = logits.to(torch.float)
         greedy_tokens = logits.argmax(dim=-1)
         logits.div_(temperatures.unsqueeze(dim=1))
         probs = torch.softmax(logits, dim=-1, dtype=torch.float)
         # logprobs = torch.log_softmax(logits, dim=-1, dtype=torch.float)
         epsilon = 1e-10  
-        sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1) + epsilon).argmax(dim=-1)  
+        sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1) + epsilon).argmax(dim=-1)
+        """
+        这里很有意思哈，
+        按照常规思路
+        一般是，if xx 计算A else 计算B
+        但这里却相反————
+        先计算A并计算B，再“判断”我要谁
+        
+        这里涉及到了GPU并行计算的特点和PyTorch执行的机制
+        GPU擅长计算，而非选择控制逻辑，所以这么干
+
+        这里还有一个点可以将那就是`torch.where`，看上去最后还是要有控制逻辑？
+        并非如此，torch.where其实还是计算逻辑，这是一种并行执行的向量化计算（无分支跳转）
+        所以他只是长得像控制逻辑
+        """  
         return torch.where(temperatures == 0, greedy_tokens, sample_tokens)
